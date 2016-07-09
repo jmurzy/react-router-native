67266ca80ec3b403c211d985e6537a2b62252e6d
Joon Ho Cho <joonho1101@gmail.com>
2016-07-09 03:28:02 -0700

    apply middleware

--- modules/applyRouterMiddleware.js
+++ modules/applyRouterMiddleware.js
@@ -5,12 +5,12 @@
  * https://github.com/reactjs/react-router/blob/master/LICENSE.md
  */
 
-import React, { createElement } from 'react';
-import RouterContext from './RouterContext';
+import React, { createElement } from 'react'
+import RouterContext from './RouterContext'
 
 export default (...middlewares) => {
-  const withContext = middlewares.map(m => m.renderRouterContext).filter(f => f);
-  const withComponent = middlewares.map(m => m.renderRouteComponent).filter(f => f);
+  const withContext = middlewares.map(m => m.renderRouterContext).filter(f => f)
+  const withComponent = middlewares.map(m => m.renderRouteComponent).filter(f => f)
   const makeCreateElement = (baseCreateElement = createElement) => (
     (Component, props) => (
       withComponent.reduceRight(
@@ -19,19 +19,19 @@ export default (...middlewares) => {
         ), baseCreateElement(Component, props)
       )
     )
-  );
+  )
 
-  return (routeDefinition) => (renderProps) => (
+  return (renderProps) => (
     withContext.reduceRight(
       (previous, renderRouterContext) => (
         renderRouterContext(previous, renderProps)
       ), (
-      <RouterContext
-        {...renderProps}
-        allRoutes={routeDefinition}
-        createElement={makeCreateElement(renderProps.createElement)}
-      />
+        <RouterContext
+          {...renderProps}
+          createElement={makeCreateElement(renderProps.createElement)}
+        />
       )
     )
-  );
-};
+  )
+}
+
